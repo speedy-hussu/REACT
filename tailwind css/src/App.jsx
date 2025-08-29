bfcac1bdf4b8c138c392824a7fec7648ba6fb9ab
speedy-hussu <speedyhussu.work@gmail.com>
2025-08-29 19:50:50 +0530

    nav and hero sec

--- tailwind css/src/App.jsx
+++ tailwind css/src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
 
 function App() {
   return (
-  <></>
+    <div className="min-h-dvh w-dvw bg-[#161616]">
+      <Nav />
+      <Hero />
+    </div>
   );
 }
 
