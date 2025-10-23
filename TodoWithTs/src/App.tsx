4381fa63d1d068544d29a0d260fbbbaa3a170b90
speedy-hussu <speedyhussu.work@gmail.com>
2025-10-24 00:47:32 +0530

    Add Sonner for toast notifications and integrate next-themes; update Todo component layout

--- TodoWithTs/src/App.tsx
+++ TodoWithTs/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import InputBox from "./components/InputBox";
 import TodosList from "./components/TodosList";
 import type { TodoType } from "./types/Todo";
+import { toast, Toaster } from "sonner";
 
 export default function App() {
   const [todos, setTodos] = useState<TodoType[]>([
@@ -9,7 +10,7 @@ export default function App() {
   ]);
   const addTodo = (todo: TodoType) => {
     setTodos((prev) => [...prev, todo]);
-    
+    toast.success("Todo added successfully");
   };
   const deleteTodo = (id: number) => {
     const filteredTodos = todos.filter((todo: TodoType) => todo.id !== id);
@@ -19,6 +20,7 @@ export default function App() {
     <div className="h-screen w-screen flex flex-col items-center bg-black">
       <InputBox addTodo={addTodo} />
       <TodosList todos={todos} deleteTodo={deleteTodo} />
+      <Toaster />
     </div>
   );
 }
--- TodoWithTs/src/components/Todo.tsx
+++ TodoWithTs/src/components/Todo.tsx
@@ -13,7 +13,7 @@ export default function Todo({ id, task, deadline, deleteTodo }: TodoProps) {
   return (
     <div className="mt-2 text-white w-full h-10 p-5 bg-black flex justify-between items-center">
       <div className="w-2/3">{task}</div>
-      <div className=" w-1/6">
+      <div className=" w-2/10">
         {deadline == 0 ? "TODAY!!" : "DEADLINE: " + deadline + " days"}
       </div>
       <Button onClick={handleDelete}>
